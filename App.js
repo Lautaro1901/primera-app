ee6030b3af25d7eba7111ba683f752bdfb94bcd2
Lautaro Caraballo <lautaro.caraballo.1922@gmail.com>
2023-02-10 14:53:29 -0300

    Primer cambio

--- App.js
+++ App.js
@@ -4,10 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola CoderHose!
-        Alumno: Lautaro 
-        Comision: 48300
-      </Text>
+      <Text>Hola CoderHose</Text>
+      <Text>Alumno: Lautaro</Text>
+      <Text>Comision: 48300</Text>
       <StatusBar style="auto" />
     </View>
   );
